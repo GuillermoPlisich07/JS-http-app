b43f11c9530e6c1da0b8ae2f71958707e2949ad0
gplisich <gplisich@somil.com.uy>
2024-01-16 12:27:16 -0300

    JavaScript http

---
+++ src/brackingbad/breakingbad-app.js
@@ -0,0 +1,41 @@
+/**
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async() => {
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+    return data[0];
+}
+
+/**
+ * @param {HTMLDivElement} element 
+ */
+
+export const BreakingbadApp = async(element) => {
+
+    document.querySelector('#app-title').innerHTML = 'Breackingbad App';
+    element.innerHTML = 'Loading...';
+
+    // const quote = await fetchQuote();
+
+    const quoteLabel = document.createElement('blockquote');
+    const authLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel, authLabel, nextQuoteButton)
+    }
+
+    nextQuoteButton.addEventListener('click', async () =>{
+        element.innerHTML = 'Loading...';
+        const quote = await fetchQuote();
+        renderQuote(quote);
+    })
+
+    fetchQuote()
+        .then(renderQuote);
+
+}
